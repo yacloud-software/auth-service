2e27cad6ccc45a952189f5bee9ef93721ca67743
Conrad Wood <cnw@conradwood.net>
2023-09-13 09:38:03 +0100

    added email_only option to cwhoami
    Change-Id: Ifc9dd1d8492e645d73d35b7f9ce70fa89447f804

--- src/golang.conradwood.net/cwhoami/cwhoami.go
+++ src/golang.conradwood.net/cwhoami/cwhoami.go
@@ -20,10 +20,16 @@ var (
 	email_only = flag.Bool("email_only", false, "if true only print my email address")
 )
 
+func isQuiet() bool {
+	return *email_only
+}
+
 func main() {
 	flag.Parse()
 	//	client.GetSignatureFromAuth()
-	fmt.Printf("You are a human being\n")
+	if !isQuiet() {
+		fmt.Printf("You are a human being\n")
+	}
 	/*
 		s := tokens.GetUserTokenParameter()
 		if s == "" && os.Getenv("GE_CTX") == "" {
@@ -43,42 +49,48 @@ func main() {
 	if *debug {
 		fmt.Printf("Got context for \"%s\"\n", auth.Description(u))
 	}
-	auth.PrintUser(auth.GetUser(ctx))
+	if !isQuiet() {
+		auth.PrintUser(auth.GetUser(ctx))
+	}
 	if *debug {
 		fmt.Printf("Calling authmanager...\n")
 	}
 	u, err := authremote.GetAuthManagerClient().WhoAmI(ctx, &common.Void{})
 	utils.Bail("failed to get user account", err)
-	fmt.Printf("Cloud  : %s\n", gc.GetCloudName())
-	fmt.Printf("UserID : %s\n", u.ID)
-	fmt.Printf("Name   : %s %s\n", u.FirstName, u.LastName)
-	fmt.Printf("Email  : %s\n", u.Email)
-	fmt.Printf("Org-ID : \"%s\"\n", u.OrganisationID)
-	/*
-		var keys [][]byte
-			if *unix || *print_keys {
-				unx, err := authremote.GetAuthManagerClient().GetUnixData(ctx, &pb.ByIDRequest{UserID: u.ID})
-				utils.Bail("failed to get unix data", err)
-				fmt.Printf("Unixid : %d\n", unx.Uid)
-				fmt.Printf("SSHkeys: %d\n", len(unx.SSHKeys))
-				keys = unx.SSHKeys
-		}
-	*/
-	fmt.Printf("Groups:\n")
-	for _, g := range u.Groups {
-		fmt.Printf("  %4s (%s)\n", g.ID, g.Name)
-	}
-	/*
-		if *print_keys && len(keys) > 0 {
-			for i, k := range keys {
-				fmt.Printf("%d. Key:\n%s\n", i+1, k)
+	if !isQuiet() {
+		fmt.Printf("Cloud  : %s\n", gc.GetCloudName())
+		fmt.Printf("UserID : %s\n", u.ID)
+		fmt.Printf("Name   : %s %s\n", u.FirstName, u.LastName)
+		fmt.Printf("Email  : %s\n", u.Email)
+		fmt.Printf("Org-ID : \"%s\"\n", u.OrganisationID)
+		/*
+			var keys [][]byte
+				if *unix || *print_keys {
+					unx, err := authremote.GetAuthManagerClient().GetUnixData(ctx, &pb.ByIDRequest{UserID: u.ID})
+					utils.Bail("failed to get unix data", err)
+					fmt.Printf("Unixid : %d\n", unx.Uid)
+					fmt.Printf("SSHkeys: %d\n", len(unx.SSHKeys))
+					keys = unx.SSHKeys
 			}
+		*/
+		fmt.Printf("Groups:\n")
+		for _, g := range u.Groups {
+			fmt.Printf("  %4s (%s)\n", g.ID, g.Name)
 		}
-	*/
-	sig := gc.VerifySignature(u)
-	if !sig {
-		fmt.Printf("WARNING: Signature is not valid, information is not trustworthy\n")
-		fmt.Printf("Signature: %v\n", u.SignatureFull)
+		/*
+			if *print_keys && len(keys) > 0 {
+				for i, k := range keys {
+					fmt.Printf("%d. Key:\n%s\n", i+1, k)
+				}
+			}
+		*/
+		sig := gc.VerifySignature(u)
+		if !sig {
+			fmt.Printf("WARNING: Signature is not valid, information is not trustworthy\n")
+			fmt.Printf("Signature: %v\n", u.SignatureFull)
+		}
 	}
-
+	if *email_only {
+		fmt.Printf("%s\n", u.Email)
+	}
 }
